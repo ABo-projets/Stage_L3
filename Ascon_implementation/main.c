1d8cdec001cbb53712aa23e7eb80b89f6a7318a5
alexane <alexane.boldo@gmail.com>
2025-07-11 09:50:57 +0200

    Adding Ascon's main

---
+++ Ascon_implementation/main.c
@@ -0,0 +1,106 @@
+#include "ascon.h"
+#include <stdint.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+//debug functions
+bool same_message(uint8_t* P, uint8_t* Pprim, int len_p, bool authenticated){
+    for (int i=0; i<len_p; i++){
+        if (P[i] != Pprim[i]){
+            return false;
+        }
+    }
+    return authenticated;
+}
+
+int diff_messages(uint8_t* P, uint8_t* Pprim, int len_p){
+    int nb = 0;
+    for(int i=0; i<len_p; i++){
+        if (P[i] != Pprim[i])
+            nb++;
+    }
+    return nb;
+}
+
+void printf_listes_8(uint8_t* lst, int len){
+    for(int i=0; i<len; i++){
+        printf("%x ",lst[i]);
+    }
+    printf("\n");
+}
+
+void printf_listes_64(uint64_t* lst, int len){
+    for(int i=0; i<len; i++){
+        printf("%lx ",lst[i]);
+    }
+    printf("\n");
+}
+
+void printf_state(State_t* state){
+    printf("x0 : %lx\n",state->words[0]);
+    printf("x1 : %lx\n",state->words[1]);
+    printf("x2 : %lx\n",state->words[2]);
+    printf("x3 : %lx\n",state->words[3]);
+    printf("x4 : %lx\n",state->words[4]);
+    printf("s : %d, t : %d\n",state->s,state->t);
+    for(int i=0; i<state->s; i++){
+        for(int j=0; j<RATIO; j++)
+            printf("%x ",state->Ai[i][j]);
+        printf("   ");
+    }
+    printf("\n");
+    for(int i=0; i<state->t; i++){
+        for(int j=0; j<RATIO; j++)
+            printf("%x ",state->blocktext_in[i][j]);
+        printf("   ");
+    }
+    printf("\n");
+}
+
+int main(int argc, char** argv){
+    int len_p = 35;
+    uint8_t* P = malloc(len_p*sizeof(uint8_t));
+    for(int i=1; i<=len_p; i++)
+        P[i-1] = i;
+
+    uint8_t* C = malloc(len_p*sizeof(uint8_t));
+
+    int len_a = 7;
+    uint8_t A[7] = {0x41,0x42,0x43,0x44,0x45,0x46,0x47};
+
+    uint8_t n[NONCE_LENGTH/BYTE_LENGTH] = {0xd7,0x9a,0x48,0xcf,0x8b,0x55,0xb7,0x37,0x84,0xfb,0x86,0x21,0xeb,0xd7,0x40,0xdd};
+
+    uint8_t key[KEY_LENGTH/BYTE_LENGTH] = {0xf5,0x29,0x6d,0x3b,0xa5,0xe2,0x10,0xc4,0x63,0xb9,0xb0,0x14,0x0b,0xcc,0x33,0x5b};
+
+    uint8_t* T = malloc(TAG_LENGTH/BYTE_LENGTH*sizeof(uint8_t));
+
+    //State_t* state = state_initialization(P,len_p,A,len_a,n,key,true); 
+    //printf_state(state);
+
+    ASCONEncrypt(C, T, P, len_p, A, len_a, n, key);
+
+    uint8_t* Pprim = malloc(len_p*sizeof(uint8_t));
+    bool fail;
+
+    ASCONDecrypt(C, Pprim, len_p, A, len_a, n, key, T, &fail);
+
+    if (same_message(P, Pprim, len_p, !fail)){
+        printf("It was well encrypted/decrypted\n");
+        printf_listes_8(C,len_p);
+        printf_listes_8(T,TAG_LENGTH/BYTE_LENGTH);
+        printf("\n");
+    }
+    else{
+        printf_listes_8(T,TAG_LENGTH/BYTE_LENGTH);
+        printf("There are %d differences is the message authenticated ? : %d\n",diff_messages(P, Pprim, len_p), !fail);
+        for(int i=0; i<len_p; i++){
+            printf(" P : %x, C : %x, Pprim : %x\n", P[i],C[i],Pprim[i]);
+        }
+    }
+
+    free(P);
+    free(C);
+    free(T);
+    free(Pprim);
+    return 0;
+}
---
+++ Ascon_implementation/main_cw.c
@@ -0,0 +1,90 @@
+#include "ascon.h"
+#include "hal/hal.h"
+#include "hal/simpleserial.h"
+#include <stdint.h>
+#include <stdlib.h>
+
+uint8_t key[KEY_LENGTH/BYTE_LENGTH];
+uint8_t A[MAX_DATA_SIZE];
+uint8_t P[MAX_DATA_SIZE];
+uint8_t C[2*MAX_DATA_SIZE];
+uint8_t* T = &(C[MAX_DATA_SIZE]);
+uint8_t n[NONCE_LENGTH];
+
+uint8_t set_key(uint8_t* k, uint8_t len)
+{
+    if (len != KEY_LENGTH/BYTE_LENGTH) {
+        led_error(1);
+        return 0x01;
+    }
+
+	for(int i = 0; i < KEY_LENGTH/BYTE_LENGTH; i++) {
+        key[i] = k[i];
+    }
+	return 0x00;
+}
+
+uint8_t set_assodata(uint8_t* ad, uint8_t len){
+    if (len != MAX_DATA_SIZE) {
+        led_error(1);
+        return 0x01;
+    }
+
+	for(int i = 0; i < MAX_DATA_SIZE; i++) {
+        A[i] = ad[i];
+    }
+	return 0x00;
+}
+
+uint8_t set_nonce(uint8_t* nonce, uint8_t len){
+    if (len != NONCE_LENGTH/BYTE_LENGTH){
+        led_error(1);
+        return 0x01;
+    }
+
+    for(int i=0; i < NONCE_LENGTH/BYTE_LENGTH; i++){
+        n[i] = nonce[i];
+    }
+    return 0x00;
+}
+
+uint8_t set_pt(uint8_t* pt, uint8_t len)
+{
+    if (len != MAX_DATA_SIZE) {
+        led_error(1);
+        return 0x01;
+    }
+
+    for(int i = 0; i < MAX_DATA_SIZE; i++) {
+        P[i] = pt[i];
+    }
+
+    /*
+    Encrypt here using ciphertext, plaintext, tag, associated data and key variables.
+    */
+	ASCONEncrypt(C,T,P,len,A,len,n,key);
+	
+	simpleserial_put('r', 16, C);//sends ciphertext
+	return 0x00;
+}
+
+int main(void)
+{
+    platform_init();
+    init_uart();
+    trigger_setup();
+
+    led_ok(1);
+    led_error(0);
+
+    simpleserial_init();
+    simpleserial_addcmd('k', 16, set_key);
+    simpleserial_addcmd('a', 16, set_assodata);
+    simpleserial_addcmd('n', 16,  set_nonce);
+    simpleserial_addcmd('p', 16,  set_pt);
+
+
+    while(1) {
+        simpleserial_get();// get next command and react to it
+    }
+}
